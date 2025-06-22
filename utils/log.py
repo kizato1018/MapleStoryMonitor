4ea7ee6f9eb2fc6f21c37bef1c41cd700dddeb38
kizato1018 <kizato1018@gmail.com>
2025-06-22 08:06:02 +0800

    v2.0.5 修正log導致程式無法使用bug

--- utils/log.py
+++ utils/log.py
@@ -13,35 +13,34 @@ if not os.path.exists(LOG_DIR):
 
 LOG_FILE = os.path.join(LOG_DIR, 'game_monitor.log')
 
-class CustomRotatingFileHandler(RotatingFileHandler):
-    """自定義的RotatingFileHandler，修改備份檔案命名格式"""
+# class CustomRotatingFileHandler(RotatingFileHandler):
+#     """自定義的RotatingFileHandler，修改備份檔案命名格式"""
     
-    def rotation_filename(self, default_name):
-        """自定義備份檔案名稱格式"""
-        dirname, basename = os.path.split(default_name)
-        prefix, ext = os.path.splitext(basename)
+#     def rotation_filename(self, default_name):
+#         """自定義備份檔案名稱格式"""
+#         dirname, basename = os.path.split(default_name)
+#         prefix, ext = os.path.splitext(basename)
         
-        # 找出現有的備份檔案數量
-        existing_files = glob.glob(os.path.join(dirname, f"{prefix}*.log"))
-        backup_number = len(existing_files)
+#         # 找出現有的備份檔案數量
+#         existing_files = glob.glob(os.path.join(dirname, f"{prefix}*.log"))
+#         backup_number = len(existing_files)
         
-        return os.path.join(dirname, f"{prefix}{backup_number}.log")
+#         return os.path.join(dirname, f"{prefix}{backup_number}.log")
 
-def clear_log_directory():
-    """清空Log資料夾"""
-    if os.path.exists(LOG_DIR):
-        for filename in os.listdir(LOG_DIR):
-            file_path = os.path.join(LOG_DIR, filename)
-            if os.path.isfile(file_path) and filename.endswith('.log'):
-                os.remove(file_path)
+# def clear_log_directory():
+#     """清空Log資料夾"""
+#     if os.path.exists(LOG_DIR):
+#         for filename in os.listdir(LOG_DIR):
+#             file_path = os.path.join(LOG_DIR, filename)
+#             if os.path.isfile(file_path) and filename.endswith('.log'):
+#                 os.remove(file_path)
 
-def setup_logging():
-    """設定日誌系統"""
-    # 清空Log資料夾
-    clear_log_directory()
+# def setup_logging():
+#     """設定日誌系統"""
+#     # 清空Log資料夾
+#     clear_log_directory()
 
-# 清空Log資料夾
-clear_log_directory()
+
 
 # 設定root logger
 logging.basicConfig(
@@ -49,13 +48,13 @@ logging.basicConfig(
     format='[%(asctime)s] [%(levelname)s] %(message)s',
     handlers=[
         logging.StreamHandler(),
-        CustomRotatingFileHandler(LOG_FILE, maxBytes=20*1024*1024, backupCount=3, encoding='utf-8')
+        RotatingFileHandler(LOG_FILE, encoding='utf-8')
     ]
 )
 
 # 設定file handler為DEBUG, console為INFO，並分別設置formatter
 for handler in logging.getLogger().handlers:
-    if isinstance(handler, (RotatingFileHandler, CustomRotatingFileHandler)):
+    if isinstance(handler, (RotatingFileHandler, RotatingFileHandler)):
         handler.setLevel(logging.DEBUG)
         handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] %(filename)s:%(lineno)d %(message)s'))
     else:
